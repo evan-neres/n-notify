35cddd64ef724f35cc20af110ed6e9d7ccb73c16
Evandro Neres <evandro.neres@proton.me>
2025-05-11 18:05:34 -0300

    regras

--- src/components/FeaturesSection.tsx
+++ src/components/FeaturesSection.tsx
@@ -15,7 +15,7 @@ const features = [
   },
   {
     icon: <Settings className="h-8 w-8 text-nnotify" />,
-    title: 'Regras personalizadas',
+    title: 'Regras',
     description: 'Defina condições de alerta e preferências de notificação adaptadas às suas necessidades.'
   },
   {
